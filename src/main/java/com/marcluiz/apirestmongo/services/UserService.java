e09f066ca3282d8a5fe86532af422858e36851fd
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-10-26 08:47:02 +0100

    Insert User using Post

--- src/main/java/com/marcluiz/apirestmongo/services/UserService.java
+++ src/main/java/com/marcluiz/apirestmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcluiz.apirestmongo.domain.User;
+import com.marcluiz.apirestmongo.dto.UserDTO;
 import com.marcluiz.apirestmongo.repository.UserRepository;
 import com.marcluiz.apirestmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-		}
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
