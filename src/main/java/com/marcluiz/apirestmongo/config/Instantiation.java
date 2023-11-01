2e0e64ee4217793b7fb925b21adbef21731858f3
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-11-01 09:37:04 +0000

    AuthorDTO to display author data

--- src/main/java/com/marcluiz/apirestmongo/config/Instantiation.java
+++ src/main/java/com/marcluiz/apirestmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.marcluiz.apirestmongo.domain.Post;
 import com.marcluiz.apirestmongo.domain.User;
+import com.marcluiz.apirestmongo.dto.AuthorDTO;
 import com.marcluiz.apirestmongo.repository.PostRepository;
 import com.marcluiz.apirestmongo.repository.UserRepository;
 
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Finally Holidays!", "I'm travelling to Ireland!", u1);
-		Post post2 = new Post(null, sdf.parse("23/03/2023"), "GMorning!", "Top of the morning to ya", u1);
-		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		//instead of inserting a User directly into the argument, you should now instantiate an AuthorDTO using the User object as a reference
+		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Finally Holidays!", "I'm travelling to Ireland!", new AuthorDTO(u1));
+		Post post2 = new Post(null, sdf.parse("23/03/2023"), "GMorning!", "Top of the morning to ya", new AuthorDTO(u1));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
