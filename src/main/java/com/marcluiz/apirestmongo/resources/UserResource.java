2c86203da71f2a35d4f1fb93e23a982387a98acf
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-11-01 10:15:57 +0000

    User's Post Endpoint

--- src/main/java/com/marcluiz/apirestmongo/resources/UserResource.java
+++ src/main/java/com/marcluiz/apirestmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marcluiz.apirestmongo.domain.Post;
 import com.marcluiz.apirestmongo.domain.User;
 import com.marcluiz.apirestmongo.dto.UserDTO;
 import com.marcluiz.apirestmongo.services.UserService;
@@ -67,5 +68,12 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	//post endpoint
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
