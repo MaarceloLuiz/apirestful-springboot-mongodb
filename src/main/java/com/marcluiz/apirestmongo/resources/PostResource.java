ec5569c84ff4dbbfebfc8a284878814532aac085
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-11-02 09:21:51 +0000

    Query Methods - Containing

--- src/main/java/com/marcluiz/apirestmongo/resources/PostResource.java
+++ src/main/java/com/marcluiz/apirestmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.marcluiz.apirestmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcluiz.apirestmongo.domain.Post;
+import com.marcluiz.apirestmongo.resources.util.URL;
 import com.marcluiz.apirestmongo.services.PostService;
 
 @RestController
@@ -22,6 +26,16 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	//if we can request in Postman a research using Strings
+	//the Strings has to be encoded(GMorning%21 = GMorning!), and our method is going to decode it
+	//example: localhost/posts/titlesearch/?text=GMorning%21
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
